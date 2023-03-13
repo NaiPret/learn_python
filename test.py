1bb219aeb6ca9acab1cf54c693caca78a2584dd2
NaiPret <ngvphu235e@gmail.com>
2023-03-13 21:50:59 +0700

    13/03/2023

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print(50 / 100)
+#Bài 5:
+
+from math import gcd
+
+m = int(input("Nhập tử số m: "))
+n = int(input("Nhập mẫu số n: "))
+
+k = gcd(m, n)
+print("Phân số", m, "/", n, "có dạng tối giản là:", m/k, "/", n/k)
