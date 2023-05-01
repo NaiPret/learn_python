b795d61f371d43bcbe75148c9fd2fa38c4bea036
NaiPret <ngvphu235e@gmail.com>
2023-05-01 18:54:43 +0700

    1/5/2023

---
+++ [DCHK2] Bài 1.py
@@ -0,0 +1,4 @@
+# Bài 1
+
+s = str(input("Nhập sâu kí tự s: "))
+print("Độ dài xâu đó là:", len(s))
---
+++ [DCHK2] Bài 2.py
@@ -0,0 +1,8 @@
+# Bài 2
+
+a = str(input("Nhập xâu kí tự a: "))
+b = str(input("Nhập xâu kí tự b: "))
+if len(a) < len(b):
+    print("Xâu có độ dài ngắn hơn là: a")
+else:
+    print("Xâu có độ dài ngắn hơn là: b")
